e23fe57b324ff1e233ba7083ac2167c1510b114f
Krzysztof Bednarski <bednarski1978@gmail.com>
2017-06-23 08:43:03 +0200

    InitialCommit

---
+++ FirmAdministartion/FirmAdministration.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirmAdministration.Web.Startup))]
+namespace FirmAdministration.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
